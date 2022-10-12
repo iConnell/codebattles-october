ff902314e019614132983abc9973808c40e949cd
McConnell <ik.ugwuanyi@gmail.com>
2022-10-12 13:31:54 +0100

    added base advocate Serializer and requirements.txt

--- base/serializers.py
+++ base/serializers.py
@@ -1,18 +1,49 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from .models import Advocate, Company
+from django.urls import reverse
+
+class BaseAdvocateSerializer(ModelSerializer):
+    class Meta:
+        model = Advocate
+        fields = [
+            'id',
+            'name',
+            'profile_pic',
+            'short_bio'
+        ]
 
 class CompanySerializer(ModelSerializer):
+    advocates = SerializerMethodField('get_advocates')
+
+    def get_advocates(self, instance):
+        query = instance.advocate_set.all()
+        advocates = BaseAdvocateSerializer(query, many=True).data
+        return advocates
+
     class Meta:
         fields = [
             'id',
             'name',
             'logo',
             'summary',
+            'advocates'
         ]
         model = Company
 
+
 class AdvocateSerializer(ModelSerializer):
     company = CompanySerializer()
+
+    def to_representation(self, instance):
+        data =  super().to_representation(instance)
+        del data['company']['summary']
+        data['company']['href'] = reverse('company-detail', kwargs={'pk': data['company']['id']})
+        data['links'] = {
+            "youtube": instance.youtube,
+            "twitter": instance.twitter,
+            "github": instance.github
+        }
+        return data
 
     class Meta:
         model = Advocate
@@ -24,5 +55,4 @@ class AdvocateSerializer(ModelSerializer):
             'long_bio',
             'advocate_years_exp',
             'company',
-            'links',
         ]
