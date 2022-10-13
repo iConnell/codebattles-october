187f3e89fb1d80573f13f3e6448e9f18429e8983
McConnell <ik.ugwuanyi@gmail.com>
2022-10-13 10:23:57 +0100

    added raiway host

--- codebattles/settings.py
+++ codebattles/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['iconnell.pythonanywhere.com', 'localhost']
+ALLOWED_HOSTS = ['iconnell.pythonanywhere.com', 'iconnell-dev-advocates.up.railway.app', 'localhost']
 
 
 # Application definition
