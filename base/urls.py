4a5268503e2669c10e3b3f3b94ee500263fee224
McConnell <ik.ugwuanyi@gmail.com>
2022-11-05 02:51:10 +0100

    added company and advocate create views

--- base/urls.py
+++ base/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import CompanyViewSet, AdvocateViewSets
 
 urlpatterns = [
-    path('companies/', CompanyViewSet.as_view({'get':'list'}), name='company-list'),
+    path('companies/', CompanyViewSet.as_view({'get':'list', 'post':'create'}), name='company-list'),
     path('companies/<int:pk>/', CompanyViewSet.as_view({'get':'retrieve'}), name='company-detail'),
-    path('advocates/', AdvocateViewSets.as_view({'get':'list'}), name="advocates-list"),
+    path('advocates/', AdvocateViewSets.as_view({'get':'list', 'post':'create'}), name="advocates-list"),
     path('advocates/<str:username>/', AdvocateViewSets.as_view({'get': 'retrieve'}))
 ]
